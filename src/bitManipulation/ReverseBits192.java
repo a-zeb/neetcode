977626f956e55e1fe2bd08a7be939293a2e013fd
Zeb, Anwar <az939c@att.com>
2023-02-13 11:59:27 -0500

    unfinished reverse bits 192

---
+++ src/bitManipulation/ReverseBits192.java
@@ -0,0 +1,26 @@
+package bitManipulation;
+
+public class ReverseBits192 {
+
+	public static void main(String[] args) {
+		int input = 1001;
+		reverseBits(input);
+	}
+
+	private static int reverseBits(int n) {
+		System.out.println("input: " + n);
+		//split n into array of ints
+		
+		//loop through ints and if 1 continue if 0 add 1 and break
+		
+		//convert to decimal
+		return n;
+	}
+	
+
+}
+
+//public class ReverseBits192 {
+//    // you need treat n as an unsigned value
+//	System.out.println("test");
+//}
